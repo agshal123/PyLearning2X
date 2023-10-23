ee5af0159107b18902e638dbd40f548d9642b0a7
agshal123 <agshal@gmail.com>
2023-10-23 18:27:05 -0400

    20th oct modified file

--- 20oct2023/Task1-Palindrome checker.py
+++ 20oct2023/Task1-Palindrome checker.py
@@ -11,7 +11,7 @@
 # else:
 #     print(f"{a} is not palindrome")
 
-#2nd Method
+# 2nd Method
 # x=''
 # for i in range(a_len - 1, -1, -1):
 #     x = x+a[i]
@@ -20,18 +20,33 @@
 # else:
 #     print(f"{a} is not palindrome")
 
-#3rd Method
+# 3rd Method
 
-def pal_checker(a):
-    a_len = int(len(a))
-    print(a_len - 1)
-    x = (a[::-1])
-    return x
+# def pal_checker(a):
+#     a_len = int(len(a))
+#     print(a_len - 1)
+#     x = (a[::-1])
+#     return x
+#
+# a = input("enter the string\n")
+# z= pal_checker(a)
+# if a==z:
+#     print(f"{a} is palindrome")
+# else:
+#     print(f"{a} is not palindrome")
 
-a = input("enter the string\n")
-z= pal_checker(a)
-if a==z:
-    print(f"{a} is palindrome")
+# 4th Method
+
+def rev_string(user_input):
+    reverse_string = ""
+    for a in user_input:
+        reverse_string = a+reverse_string
+    return reverse_string
+
+user_input=input("enter the string\n")
+z=rev_string(user_input)
+if z==user_input:
+    print("Plaindrome")
 else:
-    print(f"{a} is not palindrome")
+    print("not palindrome")
 
--- 20oct2023/Task2-Sum of digits.py
+++ 20oct2023/Task2-Sum of digits.py
@@ -9,15 +9,38 @@
 #     x = int(a[i]) + int(x)
 # print(x)
 
-#Method 2 using function
+# Method 2 using function
 
-def add_dig(a,num_len):
-    x=0
-    for i in range(0, num_len):
-        x = int(a[i]) + int(x)
-    return x
+# def add_dig(a,num_len):
+#     x=0
+#     for i in range(0, num_len):
+#         x = int(a[i]) + int(x)
+#     return x
+#
+# a= input("Enter any number\n")
+# num_len = int(len(a))
+# x= add_dig(a,num_len)
+# print(f"sum of digits in {a} is {x}")
 
-a= input("Enter any number\n")
-num_len = int(len(a))
-x= add_dig(a,num_len)
-print(f"sum of digits in {a} is {x}")
+# Method 3
+
+# def sum_of_digits(num):
+#     sum = 0
+#     for a in num:
+#         sum = sum + int(a)
+#     return sum
+#
+#
+# num = input("enter the number")
+# sum = sum_of_digits(num)
+# print(sum)
+
+# Method 4
+
+num = int(input("enter the number"))
+sum = 0
+while num != 0:
+    digit = num % 10
+    sum = sum + digit
+    num = int(num / 10)
+print(sum)
